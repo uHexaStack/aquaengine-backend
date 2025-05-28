bf20fc481621679de1192eff63ee618f004f5f6f
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-05-27 21:11:44 -0500

    feat: add @EnableJpaAuditing to main application class.

--- src/main/java/com/qu3dena/aquaengine/backend/AquaEngineBackendApplication.java
+++ src/main/java/com/qu3dena/aquaengine/backend/AquaEngineBackendApplication.java
@@ -2,8 +2,10 @@ package com.qu3dena.aquaengine.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AquaEngineBackendApplication {
 
     public static void main(String[] args) {
