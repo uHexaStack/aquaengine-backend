94cb22946c6948e95ab0ce96bd67a55ed6aba037
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-05-27 19:41:02 -0500

    chore: initial commit.

---
+++ src/main/java/com/qu3dena/aquaengine/backend/AquaEngineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qu3dena.aquaengine.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaEngineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaEngineBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qu3dena/aquaengine/backend/AquaEngineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qu3dena.aquaengine.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaEngineBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
