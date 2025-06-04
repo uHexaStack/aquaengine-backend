670a6a0bad359cefc5e8c3e887981d97b54e588b
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-06-04 12:46:59 -0500

    feat(payment): add SeedPaymentStatuses command.

---
+++ src/main/java/com/qu3dena/aquaengine/backend/payment/domain/model/commands/SeedPaymentStatusesCommand.java
@@ -0,0 +1,10 @@
+/**
+ * Command to seed payment statuses.
+ * <p>
+ * This record is used to initiate the process of
+ * seeding payment statuses into the repository.
+ */
+package com.qu3dena.aquaengine.backend.payment.domain.model.commands;
+
+public record SeedPaymentStatusesCommand() {
+}
