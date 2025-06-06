486ce95cb0a77fb3bbf9cde4644022f76b95b576
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-06-05 22:01:05 -0500

    chore(inventory): remove unused query.

--- src/main/java/com/qu3dena/aquaengine/backend/inventory/domain/model/queries/GetInventoryByProductIdQuery.java
+++
@@ -1,9 +0,0 @@
-package com.qu3dena.aquaengine.backend.inventory.domain.model.queries;
-
-public record GetInventoryByProductIdQuery(Long productId) {
-    public GetInventoryByProductIdQuery {
-        if (productId == null ) {
-            throw new IllegalArgumentException("Product ID cannot be null");
-        }
-    }
-}
